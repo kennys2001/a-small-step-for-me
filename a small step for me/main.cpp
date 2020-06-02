51dad0f7ed48d455c6f4f2b358aa8ba6b11cfc73
“kennys2001” <1851344171@qq.com>
2020-06-02 13:42:21 +0800

    Initial Commit

---
+++ a small step for me/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a small step for me
+//
+//  Created by lyz2333 on 2020/6/2.
+//  Copyright © 2020 lyz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
